8cc57b0212f77db79d103c70220cbc0a7fa9b5e4
clicktravel-toddrunham <todd.runham@clicktravel.com>
2017-11-10 09:33:49 +0000

    Updated the skeleton wireframe to include a button

--- lib/components/Loader/Skeleton/index.jsx
+++ lib/components/Loader/Skeleton/index.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 import GridWireframe from '../../List/Grid/Wireframe';
 
 const Skeleton = () => [
-  <GridWireframe />,
-  <br />,
-  <div className="loading-spinner wireframe-loader" />,
+  <div className="wireframe-container">
+    <GridWireframe />
+    <div className="loading-spinner wireframe-loader" />
+  </div>,
 ];
 
 export default Skeleton;
