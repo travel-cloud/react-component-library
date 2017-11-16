a437f66df7462efc3eb09b57fb6827a688cf5e0c
clicktravel-toddrunham <todd.runham@clicktravel.com>
2017-11-16 09:21:44 +0000

    Switched the link ability in Grid list from an a tag to a callback

--- lib/components/List/Grid/index.jsx
+++ lib/components/List/Grid/index.jsx
@@ -3,18 +3,21 @@ import PropTypes from 'prop-types';
 
 const renderListColumns = ({ keyMap, item }) => keyMap.map(column => (
   <p
-    key={`p-${item[keyMap[0].key]}-${column.key}`}
+    key={Math.random()}
     className="pull-left"
     style={{ width: '20%', marginBottom: '15px' }}
   >
     {
-        column.link ?
-          <a href={column.link.replace(':id', item[column.key])} className="text-large">
-            {item[column.key]}
-          </a>
-          :
-          <span className="text-large">{item[column.key]}</span>
-      }
+      column.onClick ?
+        // eslint-disable-next-line
+        <span
+          onClick={() => column.onClick(item[column.key])}
+          className="text-large link"
+        >
+          {item[column.key]}
+        </span> :
+        <span className="text-large">{item[column.key]}</span>
+    }
     <br />
     <span className="text-muted">{column.text || column.key}</span>
   </p>
@@ -36,7 +39,7 @@ const Grid = ({ classes, items, keyMap }) => (
 Grid.propTypes = {
   classes: PropTypes.string,
   items: PropTypes.arrayOf(PropTypes.shape({
-    link: PropTypes.string,
+    onClick: PropTypes.func,
     key: PropTypes.string,
     text: PropTypes.string,
   })).isRequired,
