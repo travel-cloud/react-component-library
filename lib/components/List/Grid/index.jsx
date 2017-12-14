3032827977a843d5b09c5a886ff8431c142d144b
Mark Earlam <mark.earlam@clicktravel.com>
2017-12-14 08:49:48 +0000

    NO_JIRA Adding the ability to create links in the Grid
    Example keyMap to make this work added to stories:
    
    keyMap={[{
                  key: 'linkDesc',
                  text: 'This is displaying a href link',
                  link: {
                    url: '/test',
                    key: 'linkKey',
                  },
                }

--- lib/components/List/Grid/index.jsx
+++ lib/components/List/Grid/index.jsx
@@ -1,31 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import flatten from 'flat';
+import { Link } from 'react-router-dom';
 import Wireframe from './Wireframe';
+
+// eslint-disable-next-line
+const renderOnClick = ({ column, flattenedItem }) => (
+  // eslint-disable-next-line
+  <span
+    onClick={() => column.onClick(flattenedItem[column.key])}
+    className="text-large link"
+  >
+    {flattenedItem[column.key]}
+  </span>
+);
+
+// eslint-disable-next-line
+const renderLink = ({ column, flattenedItem }) => {
+
+  const linkKey = column.link.key;
+  const linkUrl = `${column.link.url}/${flattenedItem[linkKey]}`;
+
+  return (
+      // eslint-disable-next-line
+      <Link to={linkUrl} className="text-large link">
+        {flattenedItem[column.key]}
+      </Link>
+  );
+};
 
 const renderListColumns = ({ keyMap, item }) => {
   const flattenedItem = flatten(item);
 
-  return keyMap.map(column => (
-    <p
-      key={Math.random()}
-      className="pull-left"
-      style={{ width: '20%', marginBottom: '15px' }}
-    >
-      {
-      column.onClick ?
-        // eslint-disable-next-line
-        <span
-          onClick={() => column.onClick(flattenedItem[column.key])}
-          className="text-large link"
-        >
-          {flattenedItem[column.key]}
-        </span> :
-        <span className="text-large">{flattenedItem[column.key]}</span>
-    }
-      <br />
-      <span className="text-muted">{column.text || column.key}</span>
-    </p>));
+  return keyMap.map((column) => {
+    // text for the link will be rendered with the action if there is one
+    const hasActions = column.onClick || column.link;
+
+    return (
+      <p
+        key={Math.random()}
+        className="pull-left"
+        style={{ width: '20%', marginBottom: '15px' }}
+      >
+        {
+        column.onClick ? renderOnClick({ column, flattenedItem }) : ''
+      }
+        {
+        column.link ? renderLink({ column, flattenedItem }) : ''
+      }
+        {
+        !hasActions ? <span className="text-large">{flattenedItem[column.key]}</span> : ''
+      }
+        <br />
+        <span className="text-muted">{column.text || column.key}</span>
+      </p>);
+  });
 };
 
 const Grid = ({
@@ -51,6 +80,10 @@ Grid.propTypes = {
   classes: PropTypes.string,
   items: PropTypes.arrayOf(PropTypes.shape({
     onClick: PropTypes.func,
+    link: PropTypes.shape({
+      url: PropTypes.string,
+      key: PropTypes.string,
+    }),
     key: PropTypes.string,
     text: PropTypes.string,
   })).isRequired,
