5748dd21228ea93d23a73c7bd3723c08432c0a20
clicktravel-toddrunham <todd.runham@clicktravel.com>
2017-12-19 17:07:25 +0000

    Added search term highlighting

--- lib/components/Form/AutoComplete/index.jsx
+++ lib/components/Form/AutoComplete/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const AutoComplete = ({ items, onClick }) => (
+const AutoComplete = ({ items, onClick, searchValue }) => (
   <ul className="autocomplete">
     {
-      // eslint-disable-next-line
-      items.map(item => <li onClick={() => onClick(item)}>{item.text}</li>)
+      items.map((item) => {
+        const formattedText = { __html: item.text.replace(searchValue, `<b>${searchValue}</b>`) };
+        // eslint-disable-next-line
+        return <li onClick={() => onClick(item)} dangerouslySetInnerHTML={formattedText} />
+      })
     }
   </ul>
 );
@@ -13,7 +16,15 @@ const AutoComplete = ({ items, onClick }) => (
 // eslint-disable-next-line
 AutoComplete.propTypes = {
   onClick: PropTypes.func.isRequired,
-  items: PropTypes.arrayOf(PropTypes.string).isRequired,
+  items: PropTypes.arrayOf(PropTypes.shape({
+    text: PropTypes.string,
+  })).isRequired,
+  searchValue: PropTypes.string,
+};
+
+// eslint-disable-next-line
+AutoComplete.defaultProps = {
+  searchValue: '',
 };
 
 export default AutoComplete;
